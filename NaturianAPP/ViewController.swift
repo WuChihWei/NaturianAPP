7d1ea34b350da6dce9fe7eecafa0768613b99e56
jordanwu <w99jordan@gmail.com>
2022-06-14 18:18:56 +0800

    Initial Commit

---
+++ NaturianAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NaturianAPP
+//
+//  Created by Jordan Wu on 2022/6/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
